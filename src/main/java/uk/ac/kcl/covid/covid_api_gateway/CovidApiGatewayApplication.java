e510fa55ec92e869ed91241cc6a59da906257568
suhailmir <suhabb@gmail.com>
2021-08-20 14:51:37 +0100

    Initial commit

---
+++ src/main/java/uk/ac/kcl/covid/covid_api_gateway/CovidApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.kcl.covid.covid_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidApiGatewayApplication.class, args);
+	}
+
+}
